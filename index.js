e655759e65b8a95f5b8472e6ca45ef10d58e4d08
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-11-18 00:09:02 +0200

    fallingAsleep

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ app.listen(process.env.PORT, () => console.log('Hello, ITC!'));
 app.use(express.static('./public'));
 
 app.get('/', function(req, res){
-    res.sendFile('./index.html')
+    res.sendFile('./index.html');
+    res.sendFile('./leopics1.jpg');
+    res.sendFile('./leopics2.jpg');
+    res.sendFile('./leopics3.jpg');
 })
 
 
