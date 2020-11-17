33b4fcfb739b8d22a9f83967b96c29780d0d366a
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-11-18 00:03:11 +0200

    fallingAsleep

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express()
 app.listen(process.env.PORT, () => console.log('Hello, ITC!'));
-app.use(express.static('/public'));
+app.use(express.static('./public'));
 
 app.get('/', function(req, res){
-    res.sendFile('/public')
+    res.sendFile('./index.html')
 })
 
 
