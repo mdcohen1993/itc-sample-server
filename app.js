5a15c113a2581ccf84d0a2b5af92e472655524e6
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-11-17 22:19:59 +0200

    wip

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express()
+app.listen(process.env.PORT, () => console.log('Hello, ITC!'));
+app.use(express.static('/public'));
+
+app.get('/', function(req, res){
+    res.sendFile('/index.html')
+})
+
+
+
+
