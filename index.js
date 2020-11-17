e35394e718e2c12d226352f9f5c85730aae2c7e7
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-11-17 23:45:29 +0200

    last

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express()
 app.listen(process.env.PORT, () => console.log('Hello, ITC!'));
-app.use(express.static('/itc-sample-server/public'));
+app.use(express.static('/public'));
 
 app.get('/', function(req, res){
-    res.sendFile('/itc-sample-server/public/index.html')
+    res.sendFile('/index.html')
 })
 
 
