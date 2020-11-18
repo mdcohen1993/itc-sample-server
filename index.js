9a0dd4d9ba6d1f3ccc1b05c0d3eae40b16c87c19
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-11-18 11:23:53 +0200

    lastForTheNight

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 let cloudinary = require('cloudinary')
 app.listen(process.env.PORT, () => console.log('Hello, ITC!'));
 app.use(express.static('./public'));
-app.use(express.static('./style.css'));
+app.use(express.static('./css'));
 
 app.get('/', function(req, res){
     res.sendFile('./index.html');
