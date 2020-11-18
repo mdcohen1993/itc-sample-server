90eab5e195c0c1901c606935fc3bf0da4e2326b7
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-11-18 10:38:06 +0200

    lastForTheNight

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ let cloudinary = require('cloudinary')
 app.listen(process.env.PORT, () => console.log('Hello, ITC!'));
 app.use(express.static('./public'));
 
-cloudinary.uploader.upload("leopics1.jpg",
-function(result) { console.log(result) });
-
-cloudinary.uploader.upload("leopics2.jpg",
-function(result) { console.log(result) })
-
-cloudinary.uploader.upload("leopics3.jpg",
-function(result) { console.log(result) })
-
 app.get('/', function(req, res){
     res.sendFile('./index.html');
 })
